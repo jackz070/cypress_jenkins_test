eb13b721e5f10f806402388bb429aacad6c91836
jackz070 <95173354+jackz070@users.noreply.github.com>
2024-05-25 21:48:02 +0200

    reporting

--- cypress/e2e/test_fail.cy.js
+++ cypress/e2e/test_fail.cy.js
@@ -1,5 +1,6 @@
 describe('template spec', () => {
   it('fails (later)', () => {
+    cy.wait(10000)
     expect(true).to.equal(false)
   })
 })
--- cypress/e2e/test_pass.cy.js
+++ cypress/e2e/test_pass.cy.js
@@ -1,5 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
+    cy.wait(10000)
     expect(true).to.equal(true)
   })
 })

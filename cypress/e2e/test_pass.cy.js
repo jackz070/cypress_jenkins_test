6045d6109e9153cb5eaaab19496197b9129a63c7
jackz070 <95173354+jackz070@users.noreply.github.com>
2024-05-26 09:44:41 +0200

    params

--- cypress/e2e/test_pass.cy.js
+++ cypress/e2e/test_pass.cy.js
@@ -2,6 +2,7 @@ describe('template spec', () => {
   it('passes', () => {
     expect(true).to.equal(true)
     expect(Cypress.env('test')).to.equal('ooga booga')
-    cy.log(process.env.message)
+    cy.log("as process env", process.env.message)
+    cy.log("as cypress env", Cypress.env('message'))
   })
 })
